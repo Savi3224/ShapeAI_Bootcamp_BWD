76b4dde7135eb05dfa52742e4f9d2b5c39ae6514
Savi3224 <75203933+Savi3224@users.noreply.github.com>
2021-06-05 20:21:09 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and Reactjs</h1>
+      <p>
+        JavaScript runs on the client side of the web, which can be used to
+        design / program how the web pages behave on the occurrence of an event.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
